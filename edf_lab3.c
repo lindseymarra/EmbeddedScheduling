85ce11acdb4743b37ec46b2028e66270fc9d8dc5
Lindsey <marralindseyj@gmail.com>
2018-11-18 21:52:14 -0500

    edf implements overload, not switching between tasks at scheduling intervals

--- edf_lab3.c
+++ edf_lab3.c
@@ -9,11 +9,8 @@ int task_finish; // time a task is complete
 int task, taskA, taskB; // integer for current task for each process
 char current_task; // current task identifier
 int nextA, nextB; // sets the next time the process should generate a task
+int leftoverB, leftoverA; // keeps track of how much of a task is left if it's interrupted
 
-int calculate_deadline(int t, int period)
-{
-    return t+period;
-}
 
 int start_task(int t, int execution_time)
 {
@@ -36,60 +33,68 @@ void main()
     printf("Input execution time for process B:\n");
     scanf("%d", &executionB);
 
-    taskA = 0;
-    taskB = 0;
+    taskA = 1;
+    taskB = 1;
     nextA = 0;
     nextB = 0;
     
     for(t=0; t<=100; t++) // the scheduler is run once every time slice to choose which is the next process to run
     {
+                
         
         if(t==nextA)
         {
             printf("task %c%d released at time %d\n", 'A', taskA, t);
-            nextA = t+periodA;
+
         }
         if(t==nextB)
         {
             printf("task %c%d released at time %d\n", 'B', taskB, t);
-            nextB = t+periodB;
+
         }
-            
 
         
         if(t==0) // skip checking if the running task is complete since neither have started yet
         {
             // calculate and then compare deadlines 
-            printf("generate A and B together at time 0\n");
-            deadlineA = calculate_deadline(t, periodA);
-            deadlineB = calculate_deadline(t, periodB);
+            deadlineA = t+periodA;
+            deadlineB = t+periodB;
             if (deadlineA < deadlineB)
             {
                 current_task = 'A';
-                taskA = taskA+1;
                 task = taskA;
                 deadline = deadlineA;
+                deadlineA = nextA+periodA;
                 task_finish = start_task(t,executionA);
-                printf("task %c%d started at time %d with deadline %d\n", current_task, task, t, nextA); 
-                deadlineA = calculate_deadline((t+executionA), periodA);
-                
-
+                if(task_finish>deadline)
+                {
+                    printf("task %c%d will miss deadline %d due to overload\n", current_task, task, deadline);
+                    break;
+                }
+                printf("task %c%d started at time %d with deadline %d\n", current_task, task, t, deadlineA); 
+                taskA = taskA+1;
+                nextA = nextA+periodA;
             }
             else
             {
                 current_task = 'B';
-                taskB = taskB+1;
+
                 task = taskB;
                 deadline = deadlineB;
+                deadlineB = nextB+periodB;
                 task_finish = start_task(t,executionB);
-                printf("task %c%d started at time %d with deadline %d\n", current_task, task, t, nextB);
-                deadlineB = calculate_deadline((t+executionB), periodB);
-
+                if(task_finish>deadline)
+                {
+                    printf("task %c%d will miss deadline %d due to overload\n", current_task, task, deadline);
+                    break;
+                }
+                printf("task %c%d started at time %d with deadline %d\n", current_task, task, t, deadlineB);
+                taskB = taskB+1;
+                nextB = nextB+periodB;
 
             }
-        
-                nextA = t + periodA; // calculate the next time A will be generated
-                nextB = t + periodB; // calculate the next time B will be generated
+
+
         }
         
         
@@ -97,25 +102,43 @@ void main()
         if(t==task_finish) // checks if the running task has finished
         {
             printf("task %c%d completed at time %d\n", current_task, task, t);  
-            if (current_task == 'A')
-            {
-                current_task = 'B';
-                taskB = taskB+1;
-                task = taskB;
-                deadline = deadlineB;
-                task_finish = start_task(t,executionB);
-                printf("task %c%d started at time %d with deadline %d\n", current_task, task, t, nextB);
-                deadlineB = calculate_deadline((t+executionB), periodB);
-            }
-            else
+            // calculate and then compare deadlines 
+            deadlineA = nextA;
+            deadlineB = nextB;
+            if (deadlineA < deadlineB)
             {
                 current_task = 'A';
-                taskA = taskA+1;
                 task = taskA;
                 deadline = deadlineA;
                 task_finish = start_task(t,executionA);
-                printf("task %c%d started at time %d with deadline %d\n", current_task, task, t, nextA);
-                deadlineA = calculate_deadline((t+executionA), periodA);
+                if(task_finish>deadlineA)
+                {
+                    printf("task %c%d will miss deadline %d due to overload\n", current_task, task, deadline);
+                    break;
+                }
+                printf("task %c%d started at time %d with deadline %d\n", current_task, task, t, deadlineA); 
+                taskA = taskA+1;
+                nextA = nextA+periodA;
+
+
+            }
+            else
+            {
+                current_task = 'B';
+
+                task = taskB;
+                deadline = deadlineB;
+                deadlineB = nextB+periodB;
+                task_finish = start_task(t,executionB);
+                if(task_finish>deadlineB)
+                {
+                    printf("task %c%d will miss deadline %d due to overload\n", current_task, task, deadline);
+                    break;
+                }
+                printf("task %c%d started at time %d with deadline %d\n", current_task, task, t, deadlineB);
+                taskB = taskB+1;
+                nextB = nextB+periodB;
+
             }
 
         }
